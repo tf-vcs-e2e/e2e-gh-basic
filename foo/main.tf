70df2d813f1a645ff1562dea0b38c7829ba68cc3
tf-vcs-testing <vcs-testing@hashicorp.com>
2022-03-20 22:55:11 -0700

    602ab42e-b385-22e3-235f-6a85ba149230

--- foo/main.tf
+++ foo/main.tf
@@ -1,3 +1,3 @@
 output "root_dir" {
-   value = "hello from root directory.testingce4160ee-76d7-dc55-a1f3-523971de95a4"
+   value = "hello from root directory.testing602ab42e-b385-22e3-235f-6a85ba149230"
 }
