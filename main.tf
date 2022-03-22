fceb8bbe6133185bbd22b4f8bc5c35d773db421b
tf-vcs-testing <93227343+tf-vcs-testing@users.noreply.github.com>
2022-03-22 14:09:45 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 output "root_dir" {
-   value = "hello from root directory9c8cc56f-181d-43cd-6b19-6f7f789afcaa"
+   value = "hello from root directory9c8cc56f-181d-43cd-6b19-6f7ftgdfdvfvbd789afcaa"
 }
